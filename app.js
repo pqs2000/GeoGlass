48c0fc0b297a77bf871a2b5d844565978e8fe666
Pedro Quintero-Sanchez <pqs2000@yahoo.com>
2022-03-29 19:11:57 -0400

    Added wiki API and Geolocation Functionality

--- app.js
+++ app.js
@@ -7,62 +7,134 @@ const errorCallback = (position) => {
     console.log(position);
 }
 
-document.getElementById("useLocationBtn").disabled = true;
-document.getElementById("useZipCode").disabled = true; 
+function init(){
+    //starting both buttons and textInput as disabled at start 
+    document.getElementById("useLocationBtn").disabled = true;
+    document.getElementById("useZipCode").disabled = true; 
+    document.getElementById("useZipCodeButton").disabled = true; 
+    document.getElementById("useZipCode").classList.add('noselect'); 
+
+    //event listeners for both radio buttons 
+    document.getElementById("useLocationRadBtn").addEventListener('click', enableLocationButton);
+    document.getElementById("useZipCodeRadBtn").addEventListener('click', enableZipCode);
+
+    //event listeners for the buttons
+    useZipCodeButton.addEventListener('click',updateValue)
+    useLocationBtn.addEventListener('click', getLocation);
+}
 
 
-document.getElementById("useLocationRadBtn").addEventListener('click', enableLocationButton);
-document.getElementById("useZipCodeRadBtn").addEventListener('click', enableZipCode);
 
-
+//this function will enable the location button once the radio button is pressed 
 function enableLocationButton() {
     document.getElementById("useLocationBtn").disabled = false;
     document.getElementById("useZipCode").disabled = true; 
+    document.getElementById("useZipCodeButton").disabled = true; 
 }
 
+//this function will enable the zipcode textbox and button once the radio button is pressed 
 function enableZipCode() {
     document.getElementById("useZipCode").disabled = false; 
     document.getElementById("useLocationBtn").disabled = true; 
+    document.getElementById("useZipCodeButton").disabled = false;
+    document.getElementById("useZipCode").classList.remove('noselect'); 
 }
 
-useZipCode.addEventListener('input',updateValue)
-useLocationBtn.addEventListener('click', getLocation);
+
 
 function getLocation() {
     console.log("The location button has been detected.");
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    geoFindMe();
+    //navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 }
 
-function updateValue(e) {
-    log.textContent = e.target.value; 
+function updateValue() {
+    document.getElementById("zipCodeError").classList.add("hidden");
+    console.log(useZipCode.value);
+    
+    if(useZipCode.value.length < 5){
+        document.getElementById("zipCodeError").classList.remove("hidden"); 
+    }
+    
 }
+function geoFindMe(){
 
+    const status = document.querySelector("#status");
 
+    function success(position){
+        const latitude = position.coords.latitude.toString(); 
+        const longitude = position.coords.longitude.toString(); 
+        status.textContent = ""; 
+        
+        
 
-async function getQuote() {
+    var url = "https://en.wikipedia.org/w/api.php"; 
 
-    spinner.classList.remove('hidden'); 
+    var params = {
+        action: "query",
+        list: "geosearch", 
+        gscoord: latitude + "|" + longitude, 
+        gsradius: "10000",
+        gslimit: "10", 
+        format: "json"
+    };
 
-    try {
-        const response = await fetch(endpoint)
-        if (!response.ok){
-            throw Error(response.statusText)
-        }
+    url = url + "?origin=*"; 
+    Object.keys(params).forEach(function(key) {url += "&" + key + "=" + params[key];})
 
-        const json = await response.json(); 
-        displayQuote(json.message); 
-    } catch (err) {
-        console.log(err)
-        alert('Failed to fetch new quote')
-    } finally {
-        newQuoteButton.disabled = false; 
-        spinner.classList.add('hidden'); 
+    fetch(url)
+        .then(function(response){return response.json();})
+        .then(function(response) {
+            var pages = response.query.geosearch; 
+            for(var place in pages) {
+                console.log(pages[place].title);
+            }
+        })
+        .catch(function(error){console.log(error)})
     }
 
+    function error(){
+        status.textContent = "Unable to retrieve your location"; 
+    }
+
+    if(!navigator.geolocation){
+        status.textContent = "Geolocation is not supported by your browser";
+    } else {
+        status.textContent = "Locating..."; 
+        navigator.geolocation.getCurrentPosition(success, error); 
+    }
 }
 
-function displayQuote(quote){
-    const quoteText = document.querySelector('#js-quote-text');
-    quoteText.textContent = quote; 
+init(); 
+
+/*
+function getPages(){
+
+
+    var url = "https://en.wikipedia.org/w/api.php"; 
+
+    var params = {
+        action: "query",
+        list: "geosearch", 
+        gscoord: latitude + " | " + longitude, 
+        gsradius: "10000",
+        gslimit: "10", 
+        format: "json"
+    };
+
+    url = url + "?origin=*"; 
+    Object.keys(params).forEach(function(key) {url += "&" + key + "=" + params[key];})
+
+    fetch(url)
+        .then(function(response){return response.json();})
+        .then(function(response) {
+            var pages = response.query.geosearch; 
+            for(var place in pages) {
+                console.log(pages[place].title);
+            }
+        })
+        .catch(function(error){console.log(error)})
 }
 
+//spinner.classList.remove('hidden'); 
+*/
