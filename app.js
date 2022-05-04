1492c3acdf4d812e9c882f031a98765fc3b9852e
Pedro Quintero-Sanchez <pqs2000@yahoo.com>
2022-05-04 14:04:03 -0400

    Fixed the Variable names, and prepared GET request for Lat and Long from Zip Code

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+
+
 const successCallback = (position) => {
     console.log(position);
 }
@@ -9,35 +11,35 @@ const errorCallback = (position) => {
 
 function init(){
     //starting both buttons and textInput as disabled at start 
-    document.getElementById("useLocationBtn").disabled = true;
-    document.getElementById("useZipCode").disabled = true; 
-    document.getElementById("useZipCodeButton").disabled = true; 
-    document.getElementById("useZipCode").classList.add('noselect'); 
+    document.getElementById("locationBtn").disabled = true;
+    document.getElementById("zipCodeInput").disabled = true; 
+    document.getElementById("zipCodeButton").disabled = true; 
+    document.getElementById("zipCodeInput").classList.add('noselect'); 
 
     //event listeners for both radio buttons 
-    document.getElementById("useLocationRadBtn").addEventListener('click', enableLocationButton);
-    document.getElementById("useZipCodeRadBtn").addEventListener('click', enableZipCode);
+    document.getElementById("locationRadio").addEventListener('click', enableLocationButton);
+    document.getElementById("zipCodeRadio").addEventListener('click', enableZipCode);
 
     //event listeners for the buttons
-    useZipCodeButton.addEventListener('click',updateValue)
-    useLocationBtn.addEventListener('click', getLocation);
+    zipCodeButton.addEventListener('click',zipCodeClick)
+    locationBtn.addEventListener('click', getLocation);
 }
 
 
 
 //this function will enable the location button once the radio button is pressed 
 function enableLocationButton() {
-    document.getElementById("useLocationBtn").disabled = false;
-    document.getElementById("useZipCode").disabled = true; 
-    document.getElementById("useZipCodeButton").disabled = true; 
+    document.getElementById("locationBtn").disabled = false;
+    document.getElementById("zipCodeInput").disabled = true; 
+    document.getElementById("zipCodeButton").disabled = true; 
 }
 
 //this function will enable the zipcode textbox and button once the radio button is pressed 
 function enableZipCode() {
-    document.getElementById("useZipCode").disabled = false; 
-    document.getElementById("useLocationBtn").disabled = true; 
-    document.getElementById("useZipCodeButton").disabled = false;
-    document.getElementById("useZipCode").classList.remove('noselect'); 
+    document.getElementById("zipCodeInput").disabled = false; 
+    document.getElementById("locationBtn").disabled = true; 
+    document.getElementById("zipCodeButton").disabled = false;
+    document.getElementById("zipCodeInput").classList.remove('noselect'); 
 }
 
 
@@ -45,19 +47,28 @@ function enableZipCode() {
 function getLocation() {
     console.log("The location button has been detected.");
     geoFindMe();
-    //navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 }
 
-function updateValue() {
+
+//user clicks the zip code button
+function zipCodeClick() {
     document.getElementById("zipCodeError").classList.add("hidden");
-    console.log(useZipCode.value);
+    console.log(zipCodeInput.value);
     
-    if(useZipCode.value.length < 5){
-        document.getElementById("zipCodeError").classList.remove("hidden"); 
+    if(zipCodeInput.value.length < 5){
+        document.getElementById("zipCodeError").classList.remove("hidden");
     }
     
+    let userZipCode = zipCodeInput.value;
+
+    geoFindMe2(userZipCode);
+
 }
+
+
 function geoFindMe(){
+
 
     const status = document.querySelector("#status");
 
@@ -98,6 +109,7 @@ function geoFindMe(){
         status.textContent = "Unable to retrieve your location"; 
     }
 
+    
     if(!navigator.geolocation){
         status.textContent = "Geolocation is not supported by your browser";
     } else {
@@ -105,6 +117,54 @@ function geoFindMe(){
         navigator.geolocation.getCurrentPosition(success, error); 
     }
 }
+
+
+function geoFindMe2(String){
+
+    const status = document.querySelector("#status");
+
+    //request for lat and long using zip code
+    const request = new XMLHttpRequest(); 
+    
+    //https://www.zipcodeapi.com/rest/DemoOnly00S9DXRS56eyOr2aU2ygCV2jKwXImAU8RvdwH8Bvmo9TfOHEaToMGxZ9/info.json/28601/degrees
+
+    request.open('GET', "https://www.zipcodeapi.com/rest/lAPeqbYnxI6yHW2cJa5tzU6ygvmGSjGleCsHCDjnNh9sukMGR9uVxmGESCOpVxT3/info.json/" 
+    + String + "/degrees");
+    request.send(); 
+
+    console.log("request with zip code " + String + " was sent");
+    /*
+    
+    var url = "https://en.wikipedia.org/w/api.php"; 
+
+    var params = {
+        action: "query",
+        list: "geosearch", 
+        gscoord: latitude + "|" + longitude, 
+        gsradius: "10000",
+        gslimit: "10", 
+        format: "json"
+    };
+
+    url = url + "?origin=*"; 
+    Object.keys(params).forEach(function(key) {url += "&" + key + "=" + params[key];})
+
+    fetch(url)
+        .then(function(response){return response.json();})
+        .then(function(response) {
+            var pages = response.query.geosearch; 
+            for(var place in pages) {
+                console.log(pages[place].title);
+                console.log(pages[place].dist)
+            }
+        })
+        .catch(function(error){console.log(error)})
+        */
+    }
+
+    
+
+
 
 init(); 
 
